3cb271e637d8ae4c54cee6f14bfa46000282bdb1
Mr.Yogesh <yogeshvanzara98@gmail.com>
2022-12-21 00:43:41 +0530

    chart height changed

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -24,8 +24,8 @@ ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 const ChartComponent = ({ data }) => {
   const chartConfigs = {
     type: "pie3d", // The chart type
-    width: "400", // Width of the chart
-    height: "400", // Height of the chart
+    width: "100%", // Width of the chart
+    height: "350", // Height of the chart
     dataFormat: "json", // Data type
     dataSource: {
       // Chart Configuration
